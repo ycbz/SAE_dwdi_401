82139f7983b27027a11393dad4b91e1ff5dec3a7
Yaël <yael.cubizolles@etu.uca.fr>
2023-03-16 17:45:00 +0100

    UPDATE: New entity "User"

---
+++ src/Entity/User.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * ListeDesUser
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="id", type="integer", nullable=true)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    public ?int $id = null;
+
+    // #[ORM\Column(length: 180, unique: true)]
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(length= 180, unique=true)
+     */
+    public ?string $email = null;
+
+    // #[ORM\Column]
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column()
+     */
+    public array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+
+
+    // #[ORM\Column]
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column()
+     */
+    public ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    /**
+     * Méthode getUsername qui permet de retourner le champ qui est utilisé pour l'authentification.
+     *
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->getUserIdentifier();
+    }
+}
